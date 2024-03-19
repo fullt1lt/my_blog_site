a5819737f3188889c105c7002dd4e6ebb19f779d
Daniil <daniiltupikin98@gmail.com>
2024-03-19 03:04:16 +0200

    Add all urls

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -16,12 +16,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from my_site.views import contacts, home, main,test
+from my_site.views import (article_delete, article_update, 
+                            contacts, create, deactivate, home, article, login, logout, profile, register, set_password, set_userdata,test,about,article_comment, 
+                            topics, topics_add, topics_remove, topics_subscribe, topics_unsubscribe)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main),
-    path('home/', home, name='index'),
+    path('about/', about),
+    path('', home, name='index'),
+    path('article/<int:article_id>/', article, name='article'),
+    path('article/<int:article_id>/comment/', article_comment, name='article_comment'),
+    path('create/', create),
+    path('article/<int:article_id>/update/', article_update, name='article_update'),
+    path('article/<int:article_id>/delete/', article_delete, name='article_delete'),
+    path('topics/', topics),
+    path('topics/<int:topic_id>/add/', topics_add, name='topics_add'),
+    path('topics/<int:topic_id>/remove/', topics_remove, name='topics_romove'),
+    path('topics/<int:topic_id>/subscribe/', topics_subscribe, name='topics_subscribe'),
+    path('topics/<int:topic_id>/unsubscribe/', topics_unsubscribe, name='topics_unsubscribe'),
+    path('profile/<str:username>/', profile, name='profile'),
+    path('set-password/', set_password, name='set_password'),
+    path('set-userdata/', set_userdata, name='set_userdata'),
+    path('deactivate/', deactivate, name='deactivate'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    
+    
     path('test/', test, name='test'),
     path('contacts/', contacts, name='contacts')
 ]
--- blog_site/my_site/views.py
+++ blog_site/my_site/views.py
@@ -1,11 +1,67 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def about(request):
+    return HttpResponse("надає звичайний текст для користувача, що описує функції сайту django.")
+
 def home(request):
     return render(request, 'index.html')
 
-def main(request):
-    return HttpResponse("Hey! It's your main view!!")
+def article(request, article_id):
+    return HttpResponse(f"Представлення деталей однієї статті. URL містить динамічну частину. Вона буде використовуватися для отримання однієї статті з бази даних #{article_id}.")
+
+def article_comment(request, article_id):
+    return HttpResponse(f"Це представлення буде використовуватися для додавання коментарів до статті. #{article_id}.")
+
+def create(request):
+    return HttpResponse("Форма створення статті.")
+
+
+def article_update(request, article_id):
+    return HttpResponse(f"Представлення для оновлення існуючих даних статті #{article_id}.")
+
+def article_delete(request, article_id):
+    return HttpResponse(f"Представлення для підтвердження видалення статті #{article_id}.")
+
+def topics(request):
+    return HttpResponse("Перелік доступних тем на сайті.")
+
+def topics_add(request, topic_id):
+    return HttpResponse(f"Додати обрану тему до списку обраних тем #{topic_id}.")
+
+def topics_remove(request, topic_id):
+    return HttpResponse(f"Видаляє вибрану тему з обраних #{topic_id}.")
+
+def topics_subscribe(request, topic_id):
+    return HttpResponse(f"Представлення для підписки на тему #{topic_id}.")
+
+def topics_unsubscribe(request, topic_id):
+    return HttpResponse(f"Представлення для відписки від теми #{topic_id}.")
+
+def profile(request, username):
+    return HttpResponse(f"Особиста сторінка користувача сайту #{username}.")
+
+def set_password(request):
+    return HttpResponse(f"Цей маршрут буде використовуватися для зміни облікових даних користувачів.")
+
+def set_userdata(request):
+    return HttpResponse(f"Цей маршрут буде використовуватися для зміни даних користувачів.")
+
+def deactivate(request):
+    return HttpResponse(f"Представлення для деактивації облікового запису (видалення).")
+
+def register(request):
+    return HttpResponse(f"Сторінка з формою для реєстрації нового користувача.")
+
+def login(request):
+    return HttpResponse(f"Сторінка з формою для логіна.")
+
+def logout(request):
+    return HttpResponse(f"Логаут. Має перенаправляти користувача назад на домашню сторінку..")
+
+
+
+
 
 def test(request):
     return render(request, 'test.html')
